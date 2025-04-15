6980060558470a84f4525892b88d908250d525ea
Assaf-Megirov <assafmegirov@gmail.com>
2025-04-15 22:29:39 +0300

    Refactor: Enabled minification with appropriate rules

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,11 +27,17 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            signingConfig = signingConfigs.getByName("debug")
+        }
+        debug {
+            isMinifyEnabled = false
+            isShrinkResources = false
         }
     }
     compileOptions {
