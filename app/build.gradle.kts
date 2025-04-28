3cca39c74bfcfe3d63139aef2ee3b2ad83722094
Assaf-Megirov <assafmegirov@gmail.com>
2025-04-28 14:47:15 +0300

    HotFix: version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.awindyendprod.storage_manager"
         minSdk = 24
         targetSdk = 35
-        versionCode = 6
-        versionName = "1.0.41"
+        versionCode = 7
+        versionName = "1.0.42"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
